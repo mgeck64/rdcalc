43ee47fbae4a90d0456de1a89df9cc648f3fcca0
Mark Geck <mgeck64@gmail.com>
2020-10-25 10:54:07 -0700

    Changed lookahead to lookahead_lexer and enclosed it in tpcalc namespace. Moved class declaration from tpcalc::parser (formerly calc_parser) .

--- lookahead.h
+++ lookahead.h
@@ -2,12 +2,49 @@
 #ifndef LOOKAHEAD_H
 #define LOOKAHEAD_H
 
-// definitions for calc_parser::lookahead
+#include "calc_lexer.h"
+#include "parse_error.h"
 
-#include "calc_parser.h"
+namespace tpcalc {
 
 template <typename CharT>
-inline auto calc_parser<CharT>::lookahead::peek_tok() -> const token& {
+class lookahead_lexer {
+// simulates two-token lookhead lexer using lexer. (implemented
+// separately from lexer to keep lexer clean and simple, and to
+// encapsulate lookahead logic)
+public:
+    using token = token<CharT>;
+    using token_ids = typename token::token_ids;
+    using radices = typename token::radices;
+    using exception = exception<CharT>;
+
+    lookahead_lexer(const CharT* input, const radices& default_radix) :
+        lexer{input, default_radix} {}
+    lookahead_lexer() = default;
+
+    auto get_tok() -> const token&; // consume a token; throws exception if token has error
+    auto cached_tok() const -> const token& {return cached_tok_;}
+
+    auto get_expected_tok(token_ids id) -> const token&;
+    // calls get_tok() to consume a token; throws exception if id doesn't
+    // match consumed token's id
+
+    auto peek_tok() -> const token&; // peek at but don't consume token
+    auto peeked_tok() const -> const token& {return peeked_tok_;}
+
+    auto peek_tok2() -> const token&; // peek at second token
+    auto peeked_tok2() const -> const token& {return peeked_tok2_;}
+
+private:
+    lexer<CharT> lexer;
+    unsigned peeked = 0;
+    token peeked_tok_ = {};
+    token peeked_tok2_ = {};
+    token cached_tok_ = {};
+};
+
+template <typename CharT>
+inline auto lookahead_lexer<CharT>::peek_tok() -> const token& {
     if (!peeked) {
         peeked_tok_ = lexer.get_tok();
         peeked = 1;
@@ -16,7 +53,7 @@ inline auto calc_parser<CharT>::lookahead::peek_tok() -> const token& {
 }
 
 template <typename CharT>
-inline auto calc_parser<CharT>::lookahead::peek_tok2() -> const token& {
+inline auto lookahead_lexer<CharT>::peek_tok2() -> const token& {
     if (peeked != 2) {
         peek_tok();
         peeked_tok2_ = lexer.get_tok();
@@ -26,7 +63,7 @@ inline auto calc_parser<CharT>::lookahead::peek_tok2() -> const token& {
 }
 
 template <typename CharT>
-inline auto calc_parser<CharT>::lookahead::get_tok() -> const token& {
+inline auto lookahead_lexer<CharT>::get_tok() -> const token& {
     if (!peeked)
         cached_tok_ = lexer.get_tok();
     else {
@@ -37,15 +74,17 @@ inline auto calc_parser<CharT>::lookahead::get_tok() -> const token& {
         }
     }
     if (cached_tok_.error != token::no_error)
-        throw parse_error(parse_error::lexer_error, cached_tok_);
+        throw exception(exception::lexer_error, cached_tok_);
     return cached_tok_;
 }
 
 template <typename CharT>
-inline auto calc_parser<CharT>::lookahead::get_expected_tok(token_ids id) -> const token& {
+inline auto lookahead_lexer<CharT>::get_expected_tok(token_ids id) -> const token& {
     if (get_tok().id != id)
-        throw parse_error(parse_error::tok_expected, cached_tok_, id);
+        throw exception(exception::tok_expected, cached_tok_, id);
     return cached_tok_;
 }
+
+} // namespace tpcalc
 
 #endif // LOOKAHEAD_H
